7ab6df3c70c7942c3df2155585812e469da1f15b
Rob Godfrey <rob_godfrey@outlook.com>
2022-08-06 10:20:58 -1000

    Initial Commit

---
+++ STEPN Sidekick/STEPN_SidekickApp.swift
@@ -0,0 +1,20 @@
+//
+//  STEPN_SidekickApp.swift
+//  STEPN Sidekick
+//
+//  Created by Rob Godfrey on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct STEPN_SidekickApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
