8b5be8e550402cb37a02d7a47d0b18e541ab4cf7
Rob Godfrey <102856686+robertgodfrey@users.noreply.github.com>
2022-10-01 22:48:44 -1000

    Adding foundation for ads

---
+++ STEPN Sidekick/Views/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  STEPN Sidekick
+//
+//  Created by Rob Godfrey on 10/1/22.
+//
+
+import Foundation
+import AppLovinSDK
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate
+{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Please make sure to set the mediation provider value to "max" to ensure proper functionality
+        ALSdk.shared()!.mediationProvider = "max"
+        
+        ALSdk.shared()!.initializeSdk { (configuration: ALSdkConfiguration) in
+            // Start loading ads
+        }
+        return true
+    }
+}
