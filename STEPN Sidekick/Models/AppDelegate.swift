13bd57f3f13d1f13548e58ec313ad1527c321bb7
Rob Godfrey <rob_godfrey@outlook.com>
2024-06-03 15:04:59 -1000

    Add more ATT stuff

--- STEPN Sidekick/Models/AppDelegate.swift
+++ STEPN Sidekick/Models/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import AppLovinSDK
+import AppTrackingTransparency
 
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
@@ -15,7 +16,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate
         guard let appLovinSdkKey: String = infoDict["AppLovinKey"] as? String else { return false }
         let initConfig = ALSdkInitializationConfiguration(sdkKey: appLovinSdkKey) { builder in
             builder.mediationProvider = ALMediationProviderMAX
-          }
+        }
+        let settings = ALSdk.shared().settings
+        settings.termsAndPrivacyPolicyFlowSettings.isEnabled = true
+        settings.termsAndPrivacyPolicyFlowSettings.privacyPolicyURL = URL(string: "https://stepnsidekick.com/privacy-policy.txt")
+
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization { status in
+                switch status {
+                    case .authorized:
+                        print("enable tracking")
+                    case .denied:
+                        print("disable tracking")
+                    default:
+                        print("disable tracking")
+                }
+            }
+        }
         // Initialize the SDK with the configuration
         ALSdk.shared().initialize(with: initConfig) { sdkConfig in
           // Start loading ads
