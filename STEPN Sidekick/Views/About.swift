f7dfb4e8d8bbee0c0beeccb93e50e59f614abb23
Rob Godfrey <rob_godfrey@outlook.com>
2024-12-24 10:56:07 -0800

    Bump version to 1.5.2

--- STEPN Sidekick/Views/About.swift
+++ STEPN Sidekick/Views/About.swift
@@ -257,7 +257,7 @@ struct About: View {
                                         .foregroundColor(Color("Link Blue"))
                                 }
                                 
-                                Text("v 1.5.1")
+                                Text("v 1.5.2")
                                     .font(Font.custom(fontHeaders, size: 13))
                                     .foregroundColor(Color("Gandalf"))
                                     .padding(.vertical, 20)
